a3511cbbf1ef14725154b7d6486f6b9bd5fb9853
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-08-04 15:58:31 -0500

    Add metadata to ERC721

--- contracts/CryptoPunks.sol
+++ contracts/CryptoPunks.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/utils/Base64.sol";
+import "./Base64.sol";
 
 contract CryptoPunks is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
@@ -22,6 +24,28 @@ contract CryptoPunks is ERC721, ERC721Enumerable {
         _safeMint(msg.sender, current);
     }
 
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns(string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "CryptoPunks #',
+                tokenId,
+                '", "description": "Crypto Punks are randomized Avataaars stored on chain to teach DApp development", "image": "',
+                "// TODO: Calculate image URL",
+                '"}'
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
     // The following functions are overrides required by Solidity.
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
