f2ac303f4bcca24df76e5a00e60c1e7abec57988
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-08-04 18:01:26 -0500

    Setting up DNA for the crypto punks

--- contracts/CryptoPunks.sol
+++ contracts/CryptoPunks.sol
@@ -9,9 +9,10 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
 import "./CryptoPunksDNA.sol";
 
-contract CryptoPunks is ERC721, ERC721Enumerable {
+contract CryptoPunks is ERC721, ERC721Enumerable, CryptoPunksDNA {
     using Counters for Counters.Counter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     Counters.Counter private _idCounter;
 
@@ -22,7 +23,57 @@ contract CryptoPunks is ERC721, ERC721Enumerable {
     function mint() public {
         uint256 current = _idCounter.current();
         require(current < maxSupply, "No more CryptoPunks left D':");
+
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
+    }
+
+    function _baseURI() internal pure override returns(string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
     }
 
     function tokenURI(uint256 tokenId)
@@ -36,17 +87,21 @@ contract CryptoPunks is ERC721, ERC721Enumerable {
             "ERC721 Metadata: URI query for nonexistent token"
         );
 
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "CryptoPunks #',
                 tokenId,
                 '", "description": "Crypto Punks are randomized Avataaars stored on chain to teach DApp development", "image": "',
-                "// TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
         return string(abi.encodePacked("data:application/json;base64,", jsonURI));
     }
+
     // The following functions are overrides required by Solidity.
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
--- contracts/CryptoPunksDNA.sol
+++ contracts/CryptoPunksDNA.sol
@@ -229,7 +229,7 @@ contract CryptoPunksDNA {
             );
     }
 
-    function _getAccesoriesType(uint8 _dna)
+    function getAccessoriesType(uint256 _dna)
         public
         view
         returns (string memory)
@@ -238,7 +238,7 @@ contract CryptoPunksDNA {
         return _accessoriesType[dnaSection % _accessoriesType.length];
     }
 
-    function _getClotheColor(uint8 _dna) public view returns (string memory) {
+    function getClotheColor(uint256 _dna) public view returns (string memory) {
         uint8 dnaSection = _getDNASection(_dna, 2);
         return _clotheColor[dnaSection % _clotheColor.length];
     }
