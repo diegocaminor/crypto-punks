2c1a9fd89ee06957d157833b399699c32b3184fa
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-08-04 01:04:34 -0500

    Handle private keys through env vars

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { DEPLOYER_SIGNER_PRIVATE_KEY, INFURA_PROJECT_ID } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: "<infura-url>",
-      accounts: ["<secret-key-metamask>"],
+      url: `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [DEPLOYER_SIGNER_PRIVATE_KEY],
     },
   },
 };
